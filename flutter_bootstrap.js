056c04ee6951867e4d6b2816ce10a95699034f1f
kii0219 <kii.s.w.t@gmail.com>
2026-01-14 08:47:48 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4107498105"
+    serviceWorkerVersion: "1653628249"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,16 +27,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "b14fce9b435043deb8aab18cb56b9fbf
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "b76e69822e714952c240a5517c1ce41d",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "091b7f04ab7792519fc928a6717c3270",
+"flutter_bootstrap.js": "5603ee03564fdd73315bf13faee0b2f8",
 "icons/Icon-192.png": "a3fc3c6a36abde48162a4987e664caa4",
 "icons/Icon-512.png": "acd6244430cc5d0abb91b25514ff2292",
 "icons/Icon-maskable-192.png": "a3fc3c6a36abde48162a4987e664caa4",
 "icons/Icon-maskable-512.png": "acd6244430cc5d0abb91b25514ff2292",
 "index.html": "ea734dbc253e9e403b7cc6164951e46e",
 "/": "ea734dbc253e9e403b7cc6164951e46e",
-"main.dart.js": "c3315510fd0b4652e178bedee7115e7c",
+"main.dart.js": "36a6102a8b5665a90117ad92a42f159b",
 "manifest.json": "2191df102fe4abcb869c925adbc1d138",
-"version.json": "a1b19e98954e9bdcfcddb613e4a941cc"};
+"version.json": "3e3b7273f55a4d2fc39ff2bfd6671f48"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
